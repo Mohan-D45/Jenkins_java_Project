4535e54c8bdd18b4eb800f71a29bf10fd79c4bef
Mohan D <mohandurai1101@gmail.com>
2026-02-09 15:48:24 +0530

    Add farewell message to Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,8 @@ class Sample{
   public static void main(String[] args){
     System.out.println("Hello Jenkins");
     System.out.println("Hello Faaaahhhhhh");
+    System.out.println("Bye Faaaahhhhhh");
   }
 }
+
 
